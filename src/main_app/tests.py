c63def445e9817eedc25d01e24fd7e9931181a49
Aleksandr Smelik <suntrope55@gmail.com>
2024-04-26 17:24:45 +0600

    fix test

--- src/main_app/tests.py
+++ src/main_app/tests.py
@@ -10,4 +10,4 @@ class SimpleTest(TestCase):
     def test_second_object_in_db(self):
         # The object was created in a migration step
         obj1 = models.SimpleModel.objects.get(pk=2)
-        self.assertEqual(obj1.just_text, 'Text 3')
+        self.assertEqual(obj1.just_text, 'Text 2')
